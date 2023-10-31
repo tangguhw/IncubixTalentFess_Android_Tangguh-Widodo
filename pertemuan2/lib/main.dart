1cfa688f0d3751ce25d6b568722f8f3111ee1cb9
20102186 <20102186@ittelkom-pwt.ac.id>
2023-10-31 17:18:15 +0700

    pertemuan 2

--- pertemuan2/lib/main.dart
+++ pertemuan2/lib/main.dart
@@ -11,13 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const HomeScreen(),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const HomeScreen());
   }
 }
 
@@ -28,44 +27,71 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-      body: Container(
-        color: Colors.green,
-        width: double.infinity,
-        child: Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
-          children: [ 
-            const Text("Pertemuan 2"),
-          Container(
-            width: 100,
-            height: 100,
-            color: Colors.amber,
-          ),
-          const SizedBox(
-            height: 28,
-          ),
-          const Text(
-            "INCUBIX",
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-          ),
-          const Text(
-            "1",
-            style: TextStyle(fontSize: 20),
-          )
-          ]
-        ]),
-        
-        children: [
-          const Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Icon(
-                Icons.abc,
-                size: 100,
-              ),
-              Icon(Icons.person)
-            ],
-          ),
-        ],
-      ),
-    ));
+            body:
+                // GridView.builder(
+                //     gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                //         crossAxisCount: 2),
+                //     itemCount: 5,
+                //     itemBuilder: (context, index) {
+                //       return Container(
+                //         margin: const EdgeInsets.all(10),
+                //         height: 100,
+                //         width: 100,
+                //         color: Colors.amber,
+                //         child: const Center(child: Text("INCUBIX")),
+                //       );
+                //     })
+
+                ListView.builder(
+                    itemCount: 15,
+                    itemBuilder: (context, index) {
+                      return Container(
+                          padding: const EdgeInsets.all(10),
+                          margin: const EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Colors.green),
+                          child: ListTile(
+                            leading: Image.network(
+                                "https://pbs.twimg.com/profile_images/1585960216054493185/ZQgjiP6a_400x400.jpg"),
+                            title: const Text(
+                              "INCUBIX TALENT FEST 2023 TEKNOLOGI",
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            subtitle: const Text("Mobile Development"),
+                            trailing: const Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [Icon(Icons.person), Icon(Icons.edit)],
+                            ),
+                          )
+
+                          // const Row(
+                          //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          //   children: [
+                          //     Row(
+                          //       children: [
+                          //         Icon(Icons.home),
+                          //         SizedBox(
+                          //           width: 10,
+                          //         ),
+                          //         Column(
+                          //           crossAxisAlignment:
+                          //               CrossAxisAlignment.start,
+                          //           children: [
+                          //             Text(
+                          //               "INCUBIX TALENT FEST 2023",
+                          //               style: TextStyle(
+                          //                   fontWeight: FontWeight.bold),
+                          //             ),
+                          //             Text("Mobile Development")
+                          //           ],
+                          //         ),
+                          //       ],
+                          //     ),
+                          //     Icon(Icons.person)
+                          //   ],
+                          // )
+                          );
+                    })));
   }
 }
