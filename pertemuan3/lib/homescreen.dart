8154e93e7eea7bf82c2e4152d91b03ce293572d2
20102186 <20102186@ittelkom-pwt.ac.id>
2023-10-31 17:45:10 +0700

    Button

---
+++ pertemuan3/lib/homescreen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Pertemuan 3"),
+        centerTitle: true,
+        backgroundColor: Colors.lightBlue,
+      ),
+      body: Column(
+        children: [
+          //Elevated Button
+          ElevatedButton(
+              // style: ElevatedButton.styleFrom(
+              // backgroundColor: Colors.amber, padding: EdgeInsets.all(30)),
+              onPressed: () {},
+              child: const Text("Elevated Button")),
+          const SizedBox(
+            height: 10,
+          ),
+          const ElevatedButton(onPressed: null, child: Text("Disable Button")),
+
+          //Outlined Button
+          OutlinedButton(
+              style: OutlinedButton.styleFrom(
+                  side: const BorderSide(color: Colors.amber)),
+              onPressed: () {},
+              child: const Text(
+                "Submit",
+                style: TextStyle(color: Colors.amber),
+              )),
+
+          //Text Button
+          TextButton(onPressed: () {}, child: const Text("Text Button"))
+        ],
+      ),
+    );
+  }
+}
