f4acc41d55602fbbae3e02104fc9cc52a88fd681
20102186 <20102186@ittelkom-pwt.ac.id>
2023-11-01 22:58:57 +0700

    belajar API

---
+++ pertemuan5/lib/model/alquran_model.dart
@@ -0,0 +1,45 @@
+class AlQuranModel {
+  int nomor;
+  String nama;
+  String namaLatin;
+  int jumlahAyat;
+  String tempatTurun;
+  String arti;
+  String deskripsi;
+  Map<String, String> audioFull;
+
+  AlQuranModel({
+    required this.nomor,
+    required this.nama,
+    required this.namaLatin,
+    required this.jumlahAyat,
+    required this.tempatTurun,
+    required this.arti,
+    required this.deskripsi,
+    required this.audioFull,
+  });
+
+  factory AlQuranModel.fromJson(Map<String, dynamic> json) => AlQuranModel(
+        nomor: json["nomor"],
+        nama: json["nama"],
+        namaLatin: json["namaLatin"],
+        jumlahAyat: json["jumlahAyat"],
+        tempatTurun: json["tempatTurun"],
+        arti: json["arti"],
+        deskripsi: json["deskripsi"],
+        audioFull: Map.from(json["audioFull"])
+            .map((k, v) => MapEntry<String, String>(k, v)),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "nomor": nomor,
+        "nama": nama,
+        "namaLatin": namaLatin,
+        "jumlahAyat": jumlahAyat,
+        "tempatTurun": tempatTurun,
+        "arti": arti,
+        "deskripsi": deskripsi,
+        "audioFull":
+            Map.from(audioFull).map((k, v) => MapEntry<String, dynamic>(k, v)),
+      };
+}
